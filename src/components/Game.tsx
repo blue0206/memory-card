0fac031f78e49c2eb9b1dc86c0de7196ce4188ec
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-22 14:13:27 +0530

    Implement card shuffling logic in Game component based on score changes

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,28 @@
-import { ReactElement } from "react";
+import { ReactElement, useState, useEffect } from "react";
+import { GameCard as PokemonCard } from "../interfaces";
 
-export default function Game(): ReactElement {
+type GameProps = {
+    mode: string;
+    score: number;
+}
+
+export default function Game({ 
+    mode, 
+    score 
+}: GameProps): ReactElement {
+    const [cards, setCards] = useState<PokemonCard[]>([]);
+
+    // Shuffle cards on score change.
+    useEffect(() => {
+        setCards(prevCards => {
+            return prevCards.map(value => ({value, sort: Math.random()}))
+                            .sort((a, b) => a.sort - b.sort)
+                            .map(({value}) => value);
+        })
+    }, [score]);
+
     return (
-        <></>
+        <div>
+        </div>
     )
 }
