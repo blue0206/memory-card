c9b5c856c57e77b2377a94b184e23eb3ff52e4a6
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-22 14:15:49 +0530

    Setup fetch logic for Pokémon images based on selected game mode

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,5 @@
 import { ReactElement, useState, useEffect } from "react";
+import { Mode } from "../types";
 import { GameCard as PokemonCard } from "../interfaces";
 
 type GameProps = {
@@ -11,6 +12,46 @@ export default function Game({
     score 
 }: GameProps): ReactElement {
     const [cards, setCards] = useState<PokemonCard[]>([]);
+    
+    // Fetch images on mode change.
+    useEffect(() => {
+        const pokemon: readonly string[] = [
+            "greninja", "pikachu", "charizard", "lucario",
+            "bulbasaur", "gengar", "snorlax", "ditto",
+            "mewtwo", "rayquaza", "sceptile", "squirtle",
+            "charmander", "vulpix", "metagross", "dragonite"
+        ];
+        setCards([]);
+        if (mode === Mode.easy) {
+            pokemon.forEach(name => {
+                fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`)
+                .then(res => res.json())
+                .then(data => {
+                    const newPokemon = {
+                        src: data.sprites.front_default,
+                        name: data.name
+                    }
+                    console.log(newPokemon);
+                    setCards(prevCards => [...prevCards, newPokemon]);
+                })
+                .catch(err => console.log(err));
+            });
+        } else {
+            fetch("https://pokeapi.co/api/v2/pokemon/pikachu/")
+            .then(res => res.json())
+            .then(data => {
+                for (let i=0; i<16; i++) {
+                    const newPokemon = {
+                        src: data.sprites.front_default,
+                        name: data.name
+                    }
+                    console.log(newPokemon);
+                    setCards(prevCards => [...prevCards, newPokemon]);
+                }
+            })
+            .catch(err => console.log(err));
+        }
+    }, [mode]);
 
     // Shuffle cards on score change.
     useEffect(() => {
