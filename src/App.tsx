82c10ed1a58fcc46d319a550bd5f9bea9c91ec67
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-22 11:14:21 +0530

    Add header and instructions for the Memory Game

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import './styles/app.style.css';
+import { useState, ReactElement, useEffect } from "react";
 
-function App() {
+function App(): ReactElement {
 
   return (
     <>
+      <header>Pocket Monsters Memory Game!</header>
+      <main>
+        <section>
+          <div>
+            Get points by clicking on a pokémon but don't click on any more than once!
+          </div>
+        </section>
+      </main>
     </>
   )
 }
 
-export default App
+export default App;
